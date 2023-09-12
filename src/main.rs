c610400cd09068d0d31b32bf5810c1150cef9f36
Jacob Birkett <spikespaz@outlook.com>
2023-09-11 23:56:01 -0700

    lib: separate from bin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-pub mod config;
-
-use config::monitors::MonitorSettings;
+use hlwsp::config::monitors::MonitorSettings;
 
 fn main() {
     println!("{}", MonitorSettings::default().to_keyword_string("eDP-1"));
