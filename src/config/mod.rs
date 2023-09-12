cc20933fad8ac246ba24110c15b0ae2e7b764bb7
Jacob Birkett <spikespaz@outlook.com>
2023-09-12 01:14:54 -0700

    config: impl basic ConfigProfile

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,6 +1,16 @@
 pub mod monitors;
 
+use std::collections::HashMap;
+
+use monitors::MonitorSettings;
 use serde::{Deserialize, Serialize};
+
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
+pub struct ConfigProfile {
+    pub profile_name: String,
+    pub monitors: HashMap<String, MonitorIdentifier>,
+    pub monitor_settings: HashMap<String, MonitorSettings>,
+}
 
 #[derive(Clone, PartialEq, Serialize, Deserialize)]
 pub struct MonitorIdentifier {
